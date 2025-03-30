c0111031065a52c97874dc12e4302696830734bb
mw <694497013@qq.com>
2025-03-30 13:56:57 +0000

    o 添加账号

--- setupEnv.sh
+++ setupEnv.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+git config --global user.name mw
+git config --global user.email 694497013@qq.com
+
 ssh-keygen -t rsa -b 4096 -C "694497013@qq.com"
 ssh-keyscan -t rsa github.com >> /root/.ssh/known_hosts
 ssh-keyscan -t rsa gitee.com >> /root/.ssh/known_hosts
