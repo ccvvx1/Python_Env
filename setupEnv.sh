e0811fd482d6e93ca361cdf4df05cf71eb34da17
tanmingwu <13802885497@139.com>
2025-03-31 08:23:16 +0800

    o 输出到文本

--- setupEnv.sh
+++ setupEnv.sh
@@ -7,4 +7,4 @@ ssh-keygen -t rsa -b 4096 -C "694497013@qq.com"
 ssh-keyscan -t rsa github.com >> /root/.ssh/known_hosts
 ssh-keyscan -t rsa gitee.com >> /root/.ssh/known_hosts
 
-cat /root/.ssh/id_rsa.pub
+cat /root/.ssh/id_rsa.pub >> /content/env.txt
